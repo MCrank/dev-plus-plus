50a9761a62216c49ce68c2d346deec69a1f150d3
MCrank <marco_mcse@hotmail.com>
2018-12-14 19:02:28 -0600

    cleanup comments

--- src/helpers/data/authRequests.js
+++ src/helpers/data/authRequests.js
@@ -1,19 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-// const authenticate = (authButton) => {
-//   let provider = '';
-//   if (authButton === 'github') {
-//     provider = new firebase.auth.GithubAuthProvider();
-//     // return firebase.auth().signInWithPopup(provider);
-//   }
-//   if (authButton === 'google') {
-//     provider = new firebase.auth.GoogleAuthProvider();
-//     // return firebase.auth().signInWithPopup(provider);
-//   }
-//   return firebase.auth().signInWithPopup(provider);
-// };
-
 const githubAuth = () => {
   const provider = new firebase.auth.GithubAuthProvider();
   return firebase.auth().signInWithPopup(provider);
