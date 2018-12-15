9adf39130ec2ebdcc72742276756458d825b0167
MCrank <marco_mcse@hotmail.com>
2018-12-14 19:20:02 -0600

    added main page components

---
+++ src/components/Graph/Graph.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Graph.scss';
+
+class Graph extends React.Component {
+  render() {
+    return (
+      <div className="Graph">
+        <h2>Graph goes here</h2>
+      </div>
+    );
+  }
+}
+
+export default Graph;
