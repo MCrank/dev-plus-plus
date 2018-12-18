955bcd61badf8314601a66d7d8d883d601dd11fe
MCrank <marco_mcse@hotmail.com>
2018-12-17 19:48:21 -0600

    refactor site layout

---
+++ src/components/Resources/Resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Resources.scss';
+
+class Resources extends React.Component {
+  render() {
+    return (
+      <div className="Resources col-6">
+        <h2>Resources Form Goes Here</h2>
+      </div>
+    );
+  }
+}
+
+export default Resources;
